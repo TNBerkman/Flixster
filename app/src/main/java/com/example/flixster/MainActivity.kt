a5df203af9645a34a9c2e2a64d1cae97ad41d95d
TNBerkman <90403029+TNBerkman@users.noreply.github.com>
2022-09-26 12:25:38 -0400

    Part 2

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -11,7 +11,7 @@ import okhttp3.Headers
 import org.json.JSONException
 
 private const val  TAG = "MainActivity"
-private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=6b18007394f676f5b4d574db035f54c6"
+private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 class MainActivity : AppCompatActivity() {
     private val movies = mutableListOf<Movie>()
     private lateinit var rvMovies: RecyclerView
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,15 +1,19 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -28,11 +32,15 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
 
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
@@ -40,8 +48,18 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
             Glide.with(context)
                 .load(movie.posterImageUrl)
                 .placeholder(R.drawable.placeholder)
+                .centerCrop()
+                .transform(RoundedCorners(30))
                 .into(ivPoster)
 
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 
